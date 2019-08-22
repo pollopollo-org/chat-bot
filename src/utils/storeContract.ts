f81eabc11cd1515dc6e7b8d61bf33d5f7a521bc6
Your Name <you@example.com>
2019-08-22 12:34:20 +0200

    notification to producer is sent when donation to contract is go

--- src/utils/storeContract.ts
+++ src/utils/storeContract.ts
@@ -1,3 +1,4 @@
+import device = require("ocore/device.js");
 import { Participant } from "./offerContract";
 
 const mariadb = require("mariadb");
@@ -99,4 +100,44 @@ export async function completeContract(applicationId: string) {
             connection.end();
         }
     }
+    await notifyProducerOfFunds(applicationId);
+}
+
+/**
+ * Method to notify producer that funds have been donated to a particular product, so that the producer can reserve the product
+ */
+export async function notifyProducerOfFunds(applicationId: string) {
+    // First we obtain the information the producer needs to know
+    let connection;
+    let product;
+    let producerDevice;
+    let price;
+    let sharedAddress;
+
+    try {
+        connection = await pool.getConnection();
+        product = await connection.query("SELECT p.Title FROM Applications a JOIN Products p ON a.ProductId = p.Id WHERE a.Id = ?",
+                                         [applicationId]);
+        price = await connection.query("SELECT Price FROM Contracts WHERE ApplicationId = ?",
+                                       [applicationId]);
+        sharedAddress =  await connection.query("SELECT SharedAddress FROM Contracts WHERE ApplicationId = ?",
+                                                [applicationId]);
+        producerDevice = await connection.query("SELECT ProducerDevice FROM Contracts WHERE ApplicationID = ?", [applicationId]);
+    } catch (err) {
+        console.log(err);
+    } finally {
+        // End the process after the job finishes
+        if (connection) {
+            connection.end();
+        }
+    }
+
+    // Now we notify the producer
+    device.sendMessageToDevice(
+        producerDevice,
+        "text",
+        `A donation has been made for \"${product}\" worth ${price}USD and funds are now stored on the smart contract ` +
+        `starting with \"${sharedAddress.substring(0, 4)}\". ` +
+        `They will be available to you when the recipient confirms reception of the product.`
+    );
 }
