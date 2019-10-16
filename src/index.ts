afac7207f6a9884b3e7948e4ab505aaa9e192240
Your Name <you@example.com>
2019-10-16 23:56:16 +0200

    removed string casting of product.Title

--- src/index.ts
+++ src/index.ts
@@ -166,12 +166,11 @@ eventBus.on("new_my_transactions", async (arrUnits) => {
                 if (contract) {
                     const product = await getProductByApplicationId(contract.ApplicationId);
                     const sharedAddress = String(contract.SharedAddress);
-                    const productTitle = String(product.Title);
 
                     device.sendMessageToDevice(
                         contract.ProducerDevice,
                         "text",
-                        `The Receiver of your product "${productTitle}" has confirmed reception. In around 15 minutes you will be able ` +
+                        `The Receiver of your product "${product.Title}" has confirmed reception. In around 15 minutes you will be able ` +
                         `to extract your payment from the contract starting with ${sharedAddress.substring(0, 4)}.`
                     );
                 }
@@ -209,15 +208,15 @@ eventBus.on("my_transactions_became_stable", async (arrUnits) => {
                 if (contract && contract.Completed === 1) {
                     const sharedAddress = String(contract.SharedAddress);
                     const product = await getProductByApplicationId(contract.ApplicationId);
-                    const productTitle = String(product.Title);
+
                     device.sendMessageToDevice(
                         contract.ProducerDevice,
                         "text",
-                        `The confirmation of reception of ${productTitle} is now final and you can withdraw the donated funds` +
+                        `The confirmation of reception of ${product.Title} is now final and you can withdraw the donated funds` +
                         ` from smart wallet starting with ${sharedAddress.substring(0, 4)} - to withdraw funds, ` +
                         `switch to this contract and use the Send-button to send the funds (${contract.Price}USD) to your main wallet.`
                         );
-                    }
+                }
 
                 // ... or did a donation become stable?
                 if (contract && contract.Completed !== 1) {
@@ -225,7 +224,6 @@ eventBus.on("my_transactions_became_stable", async (arrUnits) => {
                     await updateApplicationStatus(contract.ApplicationId, ApplicationStatus.PENDING);
                     const sharedAddress = String(contract.SharedAddress);
                     const product = await getProductByApplicationId(contract.ApplicationId);
-                    const productTitle = String(product.Title);
 
                     device.sendMessageToDevice(
                         contract.DonorDevice,
@@ -238,7 +236,7 @@ eventBus.on("my_transactions_became_stable", async (arrUnits) => {
                     device.sendMessageToDevice(
                         contract.ProducerDevice,
                         "text",
-                        `A receiver has received a donation for you product ${productTitle}, ` +
+                        `A receiver has received a donation for you product ${product.Title}, ` +
                         `and will probably pick up the product within 30 days. `
                     );
                 }
