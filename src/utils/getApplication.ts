39901b36e399411ae3ea5d1ef4a25a9c3f7c49ee
Your Name <you@example.com>
2019-10-16 15:54:12 +0200

    log messages removed CREATE IF NOT EXISTS for applications

--- src/utils/getApplication.ts
+++ src/utils/getApplication.ts
@@ -10,35 +10,14 @@ const pool = mariadb.createPool({
 });
 
 /**
- * Safely creates the products table in case it doesn't exist yet.
- */
-export async function createApplicationsTable(connection) {
-    //Start by checking if the table is there and create if not
-    await connection.query(
-        `CREATE TABLE IF NOT EXISTS \n\
-            Applications(\n\
-                Id INT, \n\
-                UserId INT, \n\
-                ProductId INT, \n\
-                Motivation TEXT, \n\
-                LastModified DATETIME, \n\
-                Status INT, \n\
-                Created DATETIME, \n\
-                PRIMARY KEY (Id), \n\
-                )
-                `
-    );
-}
-
-/**
  * Retrieves a product stored in our local database based on an Id
  */
+// tslint:disable-next-line: export-name
 export async function getApplicationById(id: string) {
     let connection;
 
     try {
         connection = await pool.getConnection();
-        await createApplicationsTable(connection);
 
         const rows = await connection.query("SELECT * FROM Applications WHERE Id =?",
                                             [id || -1]);
