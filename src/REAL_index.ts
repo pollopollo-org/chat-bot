75a14bf1e1e62cebe975cfde60297660f91dab53
Yekouri <Trashskillz@gmail.com>
2020-02-09 16:45:43 +0100

    diff stuff

--- src/REAL_index.ts
+++ src/REAL_index.ts
@@ -3,18 +3,15 @@ import db = require("ocore/db");
 import device = require("ocore/device.js");
 import eventBus = require("ocore/event_bus.js");
 import validationUtils = require("ocore/validation_utils.js");
-import wallet = require("ocore/wallet");
 
 import "./listener";
 
 import { ApplicationStatus, getContractData, setProducerInformation, updateApplicationStatus } from "./requests/requests";
 
-import { application } from "express";
 import { returnAmountOfProducers, returnAmountOfProducts, returnAmountOfReceivers } from "./requests/getCounts";
 import { state } from "./state";
 import { applicationCache, donorCache, pairingCache } from "./utils/caches";
 import { getContractByConfirmKey, getContractBySharedAddress } from "./utils/getContract";
-import { getProductByApplicationId } from "./utils/getProduct";
 import { logEvent, LoggableEvents } from "./utils/logEvent";
 import { offerContract } from "./utils/offerContract";
 import { publishTimestamp } from "./utils/publishTimestamp";
@@ -23,20 +20,20 @@ import { completeContract } from "./utils/storeContract";
 /**
  * Setup cron-jobs etc. as soon as the bot is fully booted
  */
-eventBus.on("headless_wallet_ready", () => {
+eventBus.once("headless_wallet_ready", () => {
     // Ensure that the bot checks once a day if any contracts have expired.
     cron.schedule("* 0 * * *", publishTimestamp);
     state.wallet.setupChatEventHandlers();
 
-    // publishTimestamp()
-    //    .catch(err => { console.error(err); });
+    publishTimestamp()
+        .catch(err => { console.error(err); });
 });
 
 /**
  * As soon as a new user pairs to the bot, attempt to link their device address
  * to the associated PolloPollo user, based on the pairing secret.
  */
-eventBus.on("paired", async (fromAddress, pairingSecret) => {
+eventBus.once("paired", async (fromAddress, pairingSecret) => {
     // In case the pairingSecret can be parsed as an integer, that means we're
     // dealing with a donor
     const asInt = parseInt(pairingSecret, undefined);
@@ -76,7 +73,7 @@ eventBus.on("paired", async (fromAddress, pairingSecret) => {
  * Listener that'll get triggered every time a user sends something to the chat-
  * bot.
  */
-eventBus.on("text", async (fromAddress, message) => {
+eventBus.once("text", async (fromAddress, message) => {
     const parsedText = message.toLowerCase();
     const walletAddress = message
         .trim()
@@ -154,9 +151,9 @@ eventBus.on("text", async (fromAddress, message) => {
 });
 
 /**
- * Event sent on new unstable transactions
+ * Event send once transactions become stable
  */
-eventBus.on("new_my_transactions", async (arrUnits) => {
+eventBus.once("new_my_transactions", async (arrUnits) => {
     arrUnits.forEach((unit) => {
         // Did we confirm receival of a product?
         db.query("SELECT * FROM data_feeds WHERE unit = ?", [unit], (rows) => {
@@ -164,14 +161,11 @@ eventBus.on("new_my_transactions", async (arrUnits) => {
                 const contract = await getContractByConfirmKey(row.feed_name);
 
                 if (contract) {
-                    const product = await getProductByApplicationId(contract.ApplicationId);
-                    const sharedAddress = String(contract.SharedAddress);
-
                     device.sendMessageToDevice(
                         contract.ProducerDevice,
                         "text",
-                        `The Receiver of your product "${product.Title}" has confirmed reception. In around 15 minutes you will be able ` +
-                        `to extract your payment from the contract starting with "${sharedAddress.substring(0, 4)}".`
+                        "The Receiver of your product has confirmed reception. In around 15 minutes you will be able " +
+                        "to extract your payment from the contract."
                     );
                 }
             });
@@ -187,7 +181,7 @@ eventBus.on("new_my_transactions", async (arrUnits) => {
                         contract.DonorDevice,
                         "text",
                         `Your donation of ${contract.Price}$ has now been submitted. ` +
-                        "You will receive a message once the donation has been fully processed. "
+                        "You will receive a message once the donation has been fully processed."
                     );
                 }
             });
@@ -196,55 +190,24 @@ eventBus.on("new_my_transactions", async (arrUnits) => {
 });
 
 /**
- * Event sent once transactions become stable
+ * Event send once transactions become stable
  */
-eventBus.on("my_transactions_became_stable", async (arrUnits) => {
+eventBus.once("my_transactions_became_stable", async (arrUnits) => {
     arrUnits.forEach((unit) => {
-        db.query("SELECT * FROM data_feeds WHERE unit = ?", [unit], (rows) => {
-            rows.forEach(async (row) => {
-                const contract = await getContractByConfirmKey(row.feed_name);
-
-                // Did reception of a product become stable?
-                if (contract && contract.Completed === 1) {
-                    const sharedAddress = String(contract.SharedAddress);
-                    const product = await getProductByApplicationId(contract.ApplicationId);
-
-                    device.sendMessageToDevice(
-                        contract.ProducerDevice,
-                        "text",
-                        `The confirmation of reception of "${product.Title}" is now final and you can withdraw the donated funds` +
-                        ` from smart wallet starting with "${sharedAddress.substring(0, 4)}" - to withdraw funds, ` +
-                        `switch to this contract and use the Send-button to send the funds (${contract.Price}USD) to your main wallet.`
-                    );
-                }
-            });
-        });
+        // Did a donation become stable?
         db.query("SELECT address FROM outputs WHERE unit = ?", [unit], (rows => {
             rows.forEach(async (row) => {
                 const contract = await getContractBySharedAddress(row.address);
 
-                // Did a donation become stable?
-                if (contract && contract.Completed !== 1) {
+                if (contract) {
                     await completeContract(contract.ApplicationId);
                     await updateApplicationStatus(contract.ApplicationId, ApplicationStatus.PENDING);
-                    const sharedAddress = String(contract.SharedAddress);
-                    const product = await getProductByApplicationId(contract.ApplicationId);
 
                     device.sendMessageToDevice(
                         contract.DonorDevice,
                         "text",
-                        "Your donation has now been processed. Thank you for your contribution! " +
-                        `Should the receiver not pick up the product within 30 days, ` +
-                        ` you may claim the money from the contract starting with "${sharedAddress.substring(0, 4)}".`
+                        "Your donation has now been processed. Thank you for your contribution!"
                     );
-
-                    device.sendMessageToDevice(
-                        contract.ProducerDevice,
-                        "text",
-                        `A receiver has received a donation for you product "${product.Title}", ` +
-                        `and will probably pick up the product within 30 days. `
-                    );
-
                 }
             });
         }));
---
+++ src/utils/getProduct.ts
@@ -0,0 +1,62 @@
+const mariadb = require("mariadb");
+const dbData = require("../../dbData");
+
+const pool = mariadb.createPool({
+    host: dbData.host,
+    user: dbData.user,
+    password: dbData.password,
+    database: dbData.database,
+    connectionLimit: 5
+});
+
+/**
+ * Safely creates the products table in case it doesn't exist yet.
+ */
+export async function createProductsTable(connection) {
+    //Start by checking if the table is there and create if not
+    await connection.query(
+        `CREATE TABLE IF NOT EXISTS \n\
+            Products(\n\
+                Id INT, \n\
+                Title varchar(255), \n\
+                UserId INT, \n\
+                Price INT, \n\
+                Description TEXT, \n\
+                Location varchar(255), \n\
+                Available BIT, \n\
+                Country varchar(255), \n\
+                Thumbnail LONGTEXT, \n\
+                Rank INT, \n\
+                Created DATETIME, \n\
+                PRIMARY KEY (Id), \n\
+            ) \n\
+            ADD KEY (UserId)`
+    );
+}
+
+/**
+ * Retrieves a product stored in our local database based on an Id
+ */
+export async function getProductByApplicationId(applicationId: string) {
+    let connection;
+
+    try {
+        connection = await pool.getConnection();
+        /**
+         * this breaks the database for some reason
+         * await createProductsTable(connection);
+         */
+
+        const rows = await connection.query("SELECT * FROM Products WHERE Id IN " +
+                                            "(SELECT ProductId FROM Applications WHERE Id = ?)",
+                                            [applicationId || -1]);
+
+        if (rows && rows.length > 0) {
+            return rows[0];
+        }
+    } catch (err) {
+        console.error(err);
+    } finally {
+        connection.close();
+    }
+}
