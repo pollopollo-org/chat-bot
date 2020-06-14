424bdb53182cd6c708e80001d8a06a0969cec6da
Casper <casper@pollopollo.org>
2020-06-14 22:57:27 +0200

    Update Contract's Bytes

--- src/utils/withdrawToParticipant.ts
+++ src/utils/withdrawToParticipant.ts
@@ -1,25 +1,56 @@
 import { state } from "../state";
 import { getContractByApplicationId } from "./getContract";
 import { Participant } from "./offerContract";
+import { logEvent, LoggableEvents } from "./logEvent";
+
+const mariadb = require("mariadb");
+const dbData = require("../../dbData");
+
+const pool = mariadb.createPool({
+    host: dbData.host,
+    user: dbData.user,
+    password: dbData.password,
+    database: dbData.database,
+    connectionLimit: 5
+});
 
 /**
- * Withdraw funds to the Producer
+ * Function allowing a participant of a SharedAddress to request the chatbot to withdraw.
+ * The conditions of the smart contract need not be taken into consideration, as it will
+ * result in an error that gets logged in case someone tries to withdraw from a contract
+ * where the conditions aren't met (applicant didn't confirm receipt yet)
  */
-export async function withdrawToProducer(applicationId: string, recipient: Participant) {     
+export async function withdrawToParticipant(applicationId: string, recipient: Participant) {     
     
+    logEvent(LoggableEvents.UNKNOWN, { error: "Withdrawal requested for Application " + applicationId});
+
     // First retrieve the contract details based on applicationId
     let contract = await getContractByApplicationId(applicationId);
     
     if (!contract) {
-        throw new Error(`Application with ID ${applicationId} not found`);
+        logEvent(LoggableEvents.UNKNOWN, { error: "Application with ID " + applicationId + " not found!"});
     } else {
-        await state.wallet.sendAllBytesFromAddress(contract.SharedAddress, recipient.walletAddress, recipient.deviceAddress, (err, unit) => {
+        await state.wallet.sendAllBytesFromAddress(contract.SharedAddress, recipient.walletAddress, recipient.deviceAddress, async (err, unit) => {
             if (err) {
-                return callback(`Failed to make payment from ${contract.SharedAddress} to ${recipient.walletAddress}: ${err}`);
+                logEvent(LoggableEvents.UNKNOWN, { error: "Error trying to send all Bytes from sharedAddress " + contract.SharedAddress + " to user with wallet address: " + recipient.walletAddress + ": " + err});
             }
-            // Update the contract's Bytes
-            // TODO - IMPLEMENT CALL
-            callback(null, unit);
+
+            logEvent(LoggableEvents.UNKNOWN, { error: "Withdraw successful - Payment can be found in this unit: " + unit});
+            let connection;
+            try {
+                connection = await pool.getConnection();
+                await connection.query("UPDATE Constracts SET Bytes = 0 WHERE applicationId = ?", [applicationId]);
+                logEvent(LoggableEvents.UNKNOWN, {error: "Contract updated to show 0 Bytes"});
+
+            } catch (err) {
+                logEvent(LoggableEvents.UNKNOWN, { error: err });
+            } finally {
+                if (connection) {
+                    connection.end();
+                }
+            }
+            logEvent(LoggableEvents.UNKNOWN, {error: "Withdrawal for application " + applicationId + " completed"});
+            return (unit);
         });
     }
 }
