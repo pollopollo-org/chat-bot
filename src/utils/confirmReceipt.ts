88c1835c00891cb60c70740cde1f1fcba397af22
Casper <casper@pollopollo.org>
2020-09-19 23:32:35 +0200

    Handle confirms

---
+++ src/utils/confirmReceipt.ts
@@ -0,0 +1,18 @@
+import { aaConfirm } from "./aainteraction";
+
+/**
+ * Trigger the AA when Reciever confirms receipt of
+ * a product so money gets sent to the Producer
+ */
+export async function confirmReceipt(applicationId: string) {
+
+    await aaConfirm(applicationId, async (err, unit) => {
+        if (err) {
+            console.error("Error trying to confirm receipt for application: " + applicationId +
+                "\n error message: " + err.message);
+        } else {
+            console.error("Successfully confirmed receipt for application: " + applicationId);
+            console.error("Unit sent to the AA: " + unit);
+        }
+    });
+}
