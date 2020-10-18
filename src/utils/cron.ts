0bb2e02d368191e9312170874dc8d4d85f44fbc3
raizor <raizor@gmail.com>
2020-10-18 14:21:38 +0100

    Fix up linting issues in new code.
    "max-func-body-length" and "no-void-expression" linting rules have been disabled for "aainteractions.ts" and "index.ts".
    
    This is something of a workaround and should be reviewed and the related issues with the code addressed accordingly.

--- src/utils/cron.ts
+++ src/utils/cron.ts
@@ -1,7 +1,8 @@
 const mariadb = require("mariadb");
 const dbData = require("../../dbData");
 
-const sqlite = require("sqlite3").verbose();
+const sqlite = require("sqlite3")
+.verbose();
 
 const fs = require("fs");
 const path = require("path");
@@ -21,7 +22,7 @@ const pool = mariadb.createPool({
 
 const obyte = new sqlite.Database("../../.config/chat-bot/byteball.sqlite", sqlite.OPEN_READONLY, (err) => {
     if (err) {
-        console.log()
+        console.log();
     }
 });
 
@@ -35,23 +36,23 @@ const logFile = path.resolve("/home/pollopollo/.pollo_log");
 export async function handleStaleApplications() {
 
     try {
-        let conn = await pool.getConnection();
+        const conn = await pool.getConnection();
 
         // Start by retrieving all locked applicaitons
         const rows = await conn.query("SELECT * FROM Applications WHERE Status = 1");
 
         // And then go through all applications and determine whether or not
         // they've went stale.
-        for (let i = 0; i < rows.length; i++) {
-            const application = rows[i];
+        for (const r of rows) {
+            const application = rows[r];
             const lastModified = new Date(application.LastModified).getTime();
             const isStale = Date.now() - (1000 * 60 * 60) > lastModified;
             if (isStale) {
                 await conn.query("UPDATE Applications SET Status = 0, LastModified = NOW() WHERE Id = ?", [application.Id]);
-                
+
                 const contract = conn.query("SELECT * FROM Contracts where ApplicationId = ?", [application.Id]);
 
-                let logContract = `[${new Date().toUTCString()} - FOUND_STALE_CONTRACT] for application with id '${application.Id}' ` +
+                const logContract = `[${new Date().toUTCString()} - FOUND_STALE_CONTRACT] for application with id '${application.Id}' ` +
                                     `with Completed '${contract.Completed}' and confirm_key '${contract.ConfirmKey}'`;
 
                 if (await exists(logFile)) {
@@ -73,7 +74,7 @@ export async function handleStaleApplications() {
                     await release();
                 } else {
                     await createFile(logFile, message);
-                } 
+                }
             }
         }
         if (conn) {
@@ -81,68 +82,74 @@ export async function handleStaleApplications() {
         }
     } catch (err) {
         throw err;
-    } 
+    }
 }
 
-/*
+/**
  * Get the balance of a wallet address
  */
 async function getbalance(sharedWallet) {
-    return new Promise(function(resolve, reject) {
-            obyte.get("select sum(amount) as amount from outputs where is_spent=0 and asset is null and address = ?", sharedWallet, (err, row) => {
-                    if (err) reject(err.message);
-                    else resolve(row.amount);
+    return new Promise((resolve, reject) => {
+            obyte.get(
+                "select sum(amount) as amount from outputs where is_spent=0 and asset is null and address = ?",
+                sharedWallet, (err, row) => {
+                if (err) {
+                    reject(err.message);
+                } else {
+                    resolve(row.amount);
+                }
             });
-    });
+        });
 }
 
-
-/*
+/**
  * Update Contract and Application to reflect the change
  */
 export async function updateWithdrawnDonations() {
 
-    let conn = await pool.getConnection();
+    const conn = await pool.getConnection();
     // First - Find all pending applications
     let rows = await conn.query("SELECT a.Id, c.SharedAddress, ua.email as recipientemail, up.email as produceremail FROM Applications a left join Contracts c on a.Id = c.ApplicationId left join Users ua on a.UserId = ua.Id left join Products p on a.ProductId = p.Id left join Users up on p.UserId = up.Id WHERE a.Status=2;");
 
-    for (let i = 0; i < rows.length; i++) {
-        let walletbalance = await getbalance([rows[i].SharedAddress]); // Get wallet balance
+    for (const r of rows) {
+        const row = rows[r];
+        const walletbalance = await getbalance([row.SharedAddress]); // Get wallet balance
         if (!walletbalance) { // If the balance is zero on a pending application, the donor must have withdrawn the donated funds
 
             // Update the contract to reflect that Bytes has been withdrawn
-            await conn.query("UPDATE Contracts set Bytes = 0 where ApplicationId = ?", [rows[i].Id]);
+            await conn.query("UPDATE Contracts set Bytes = 0 where ApplicationId = ?", [row.Id]);
             // Update the Application to status 5 meaning WITHDRAWN
-            await conn.query("UPDATE Applications set Status = 5, LastModified = NOW() where Id = ?", [rows[i].Id]);
-                
+            await conn.query("UPDATE Applications set Status = 5, LastModified = NOW() where Id = ?", [row.Id]);
+
             // Log that we updated Applications and Contracts
-            let message = "Donor withdrew from ApplicationId " + rows[i].Id + ". Updating Application.Status to 5 and Contract.Bytes to 0";
+            const message = `Donor withdrew from ApplicationId ${row.Id}. Updating Application.Status to 5 and Contract.Bytes to 0`;
             if (await exists(logFile)) {
                 const release = await lockfile.lock(logFile);
                 await appendFile(logFile, `\n\n${message}`);
                 await release();
             } else {
                 await createFile(logFile, message);
-            } 
+            }
         }
-    };
+    }
 
     // Next - we clean up all Contracts where Producer withdrew funds by setting Bytes = 0
     rows = await conn.query("SELECT a.Id, c.SharedAddress, ua.email as recipientemail, up.email as produceremail FROM Applications a left join Contracts c on a.Id = c.ApplicationId left join Users ua on a.UserId = ua.Id left join Products p on a.ProductId = p.Id left join Users up on p.UserId = up.Id WHERE a.Status=3 and c.Bytes > 0;");
 
-    for (let i = 0; i < rows.length; i++) {
-        let walletbalance = await getbalance([rows[i].SharedAddress]); // Get wallet balance
+    for (const r of rows) {
+        const row = rows[r];
+        const walletbalance = await getbalance([row.SharedAddress]); // Get wallet balance
         if (!walletbalance) {
-            await conn.query("UPDATE Contracts set Bytes = 0 where ApplicationId = ?", [rows[i].Id]);
+            await conn.query("UPDATE Contracts set Bytes = 0 where ApplicationId = ?", [row.Id]);
             // Log that we updated Contracts
-            let message = "Producer withdrew from ApplicationId " + rows[i].Id + ". Updating Contracts.Bytes to 0";
+            const message = `Producer withdrew from ApplicationId ${row.Id}. Updating Contracts.Bytes to 0`;
             if (await exists(logFile)) {
                 const release = await lockfile.lock(logFile);
                 await appendFile(logFile, `\n\n${message}`);
                 await release();
             } else {
                 await createFile(logFile, message);
-            } 
+            }
         }
     }
 
